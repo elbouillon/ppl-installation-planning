e60c34ecf69f38b56fcaec27c603f0edf2836743
Mickael Kurmann <mickael.kurmann@gmail.com>
2012-12-01 22:35:18 +0100

    added signin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 PplInstallationPlanning::Application.routes.draw do
-  root :to => "home#index"
+  root :to => "sessions#register"
   resources :users, :only => [:index, :show, :edit, :update ]
   match '/auth/:provider/callback' => 'sessions#create'
   match '/signin' => 'sessions#new', :as => :signin
   match '/signout' => 'sessions#destroy', :as => :signout
   match '/auth/failure' => 'sessions#failure'
+
+  match '/home' => 'home#index'
 
   resources :installations, only: [:index]
   match '/installations/move' => 'installations#move'
