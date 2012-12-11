e48fd3b622b3ec66eaf03a54fa2bed0739aabf74
Mickael Kurmann <mickael.kurmann@gmail.com>
2012-12-11 21:23:30 +0100

    added a parameter to enable translation from phrase

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+
+  before_filter :check_phrase
+
+  def check_phrase
+    if params['t']
+      session['translate'] = params['t'] == 'a' ? true : false
+    end
+  end
 end
